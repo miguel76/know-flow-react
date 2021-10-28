48c00895a1b099ee425862d4499bb56e0f6a4759
Miguel Ceriani <miguel.ceriani@gmail.com>
2021-10-28 17:02:32 +0200

    Fix typescript version and setup example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import React from "react";
+import {ComponentBuilder, Engine} from "know-flow-react";
+import {newEngine as newComunicaEngine} from '@comunica/actor-init-sparql';
 
-import { ExampleComponent } from 'know-flow-react'
-import 'know-flow-react/dist/index.css'
+let options = {
+    prefixes: {
+        'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+        'rdfs': 'http://www.w3.org/2000/01/rdf-schema#',
+        'dbo': 'http://dbpedia.org/ontology/',
+        'dbr': 'http://dbpedia.org/resource/',
+        'wd': 'http://www.wikidata.org/entity/',
+        'wdt': 'http://www.wikidata.org/prop/direct/'
+    }
+};
 
-const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
-}
+let kf = new ComponentBuilder(options);
 
-export default App
+// let task = nodeToFlow(taskFactory,
+//   <kf.Input bindings='wd:Q3504248'>
+//     <p>Una lista di ... </p>
+//     <kf.Value path='rdfs:label' lang='it'/>
+//     <p>ecco ... </p>
+//     <kf.ForEach path='^wdt:P31'>
+//       <p>pianeta</p>
+//       <kf.Value path='rdfs:label' lang='it'/>
+//     </kf.ForEach>
+//   </kf.Input>
+// );
+
+// let json = JSON.stringify(stringifyFlow(task), null, 4);
+
+const TestComponent:React.FC = (props: any) => (
+  <Engine engine={newComunicaEngine()}
+      queryContext={{sources: [{ type: 'sparql', value: 'https://query.wikidata.org/sparql' }]}}>
+    {/* <p>{json}</p> */}
+    <kf.Input bindings='wd:Q3504248'>
+      <p>Una lista di ... </p>
+      <kf.Value path='rdfs:label' lang='it'/>
+      <p>eccoci ... </p>
+      <kf.ForEach path='^wdt:P31'>
+        <p>pianeta</p>
+        <kf.Value path='rdfs:label' lang='it'/>
+      </kf.ForEach>
+    </kf.Input>
+  </Engine>
+);
+  
+export default TestComponent;
+
